c29ad965b3a05051b82ef79eb94665e7339ca6ab
sks <sahusumit999@gmail.com>
2019-05-31 23:25:37 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var SimpleCrypto = require("simple-crypto-js").default;     
+var _secretKey = SimpleCrypto.generateRandom(256);  
+var User = require('../db/User');
+
+/* GET home page. */
+
+
+module.exports = function (passport) {
+    router.post('/signup', function (req, res) {
+        var body = req.body,
+            username = body.username,
+            password = body.password;
+        User.findOne({
+            username: username
+        }, function (err, doc) {
+            if (err) {
+                res.status(500).send('error occured')
+            } else {
+                if (doc) {
+                    res.status(500).send('Username already exists')
+                } else {
+                    var record = new User()
+                    record.username = username;
+                    record.secretkey = _secretKey;
+                    record.password = record.hashPassword(password)
+                    record.save(function (err, user) {
+                        if (err) {
+                            res.status(500).send('db error')
+                        } else {
+                            res.redirect('/login')
+                        }
+                    })
+                }
+            }
+        })
+    });
+
+
+    router.post('/login', passport.authenticate('local', {
+        failureRedirect: '/login', 
+        successRedirect: '/profile',
+    }), function (req, res) {
+        res.send('hey')
+    })
+    return router;
+};
